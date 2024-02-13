00e40455381e57a21b574a7376b240f2b13adaff
ljelectrar <ljelectrar@gmail.com>
2024-02-13 19:53:17 -0300

    Using and then demonstre the correct and incorrect way to approach useState

--- src/App.js
+++ src/App.js
@@ -9,13 +9,21 @@ const messages = [
 export default function App() {
 
   const [step, setStep] = useState(1);
+  const [test, setTest] = useState({name: "Leandro"});
 
   function handlePrevious (){
     if(step > 1) setStep(step - 1);
+    setTest({name: "Leandro"});
   }
 
   function handleNext (){
-    if(step < 3) setStep(step + 1);
+    
+    if(step < 3) setStep(step + 1); 
+    // BAD PRACTICE to override a var because States always must to be trated as immutable
+    // test.name = "Junior"
+
+    // GOOD PRACTICE
+    setTest({name: "Junior"}); 
   }
 
   return <div className="steps">
@@ -24,7 +32,9 @@ export default function App() {
       <div className={step >= 2 ? "active" : ""}>2</div>
       <div className={step >= 3 ? "active" : ""}>3</div>
     </div>
-    <p className="message">Step {step}: {messages[step -1]} </p>
+    <p className="message">Step {step}: {messages[step -1]} 
+      {test.name}
+    </p>
     <div className="buttons">
       <button style={{backgroundColor: '#7950f2', color: '#fff'}} 
         onClick={handlePrevious}>
