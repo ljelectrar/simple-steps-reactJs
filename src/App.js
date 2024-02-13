e969549758cf3a43153a8bb1c11a212f745c32fa
ljelectrar <ljelectrar@gmail.com>
2024-02-13 09:16:33 -0300

    using function to handle an event

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ export default function App() {
 
   const step = 3;
 
+  function handlePrevious (){
+    alert("Hello World");
+  }
+
+  function handleNext (){
+    alert("Hello World");
+  }
+
   return <div className="steps">
     <div className="numbers">
       <div className={`${step >= 1 ? "active" : ""}`}>1</div>
@@ -17,8 +25,7 @@ export default function App() {
     <p className="message">Step {step}: {messages[step -1]} </p>
     <div className="buttons">
       <button style={{backgroundColor: '#7950f2', color: '#fff'}} 
-        onClick={()=>alert('Previous')} 
-        onMouseEnter={ ()=> alert("test")}>
+        onClick={handlePrevious}>
         Previous
       </button>
       <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={()=> alert('Next')}>Next</button>
