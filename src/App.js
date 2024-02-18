7be2fe4d3060e8843696e5c2011fb100a25420d1
ljelectrar <ljelectrar@gmail.com>
2024-02-18 19:18:18 -0300

    Added a show button like a responsive menu.

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ const messages = [
 export default function App() {
 
   const [step, setStep] = useState(1);
-  const [test, setTest] = useState({name: "Leandro"});
+  //const [test, setTest] = useState({name: "Leandro"});
+
+  // Creating a new Piece of State
+  const[isOpen, setIsOpen] = useState(true);
 
   function handlePrevious (){
     if(step > 1) setStep(step - 1);
-    setTest({name: "Leandro"});
+    //setTest({name: "Leandro"});
   }
 
   function handleNext (){
@@ -23,25 +26,31 @@ export default function App() {
     // test.name = "Junior"
 
     // GOOD PRACTICE
-    setTest({name: "Junior"}); 
+    //setTest({name: "Junior"}); 
   }
 
-  return <div className="steps">
-    <div className="numbers">
-      <div className={step >= 1 ? "active" : ""}>1</div>
-      <div className={step >= 2 ? "active" : ""}>2</div>
-      <div className={step >= 3 ? "active" : ""}>3</div>
-    </div>
-    <p className="message">Step {step}: {messages[step -1]} 
-      {test.name}
-    </p>
-    <div className="buttons">
-      <button style={{backgroundColor: '#7950f2', color: '#fff'}} 
-        onClick={handlePrevious}>
-        Previous
-      </button>
-      <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handleNext}>Next</button>
-    </div>
 
+  return <div>
+    <button className="close" onClick={()=> setIsOpen(!isOpen)}>&times;</button>
+    { isOpen && (
+      <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">Step {step}: {messages[step -1]} 
+      </p>
+      <div className="buttons">
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}} 
+          onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handleNext}>Next</button>
+      </div>
+  
+    </div>
+    )
+    }
   </div>
 }
